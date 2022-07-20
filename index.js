0fa1b018941f6d7c9a1dca9ea9d562d04a4a3429
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-07-20 15:42:09 -0400

    adding cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
 const SECRET = "FDXpEaMfmnr1cS1aqhWE"
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.json({msg: 'Hola'})
