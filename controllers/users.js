c13ee35a6e8cb09ab6c198aea3559f88c752e6ce
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-07-26 12:36:53 -0400

    adding http request in controller users.js edit data

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const SALT = bcrypt.genSaltSync(10);
-  
+
 const registerUser = (req, res) => {
   const data = {
     email: req.body.email,
@@ -16,7 +16,6 @@ const registerUser = (req, res) => {
     .save()
     .then((response) => {
       res.status(200).json({ success: true, message: 'Usuario registrado con éxito' })
-
     })
     .catch((error) => {
       res.status(400).json({ success: false, message: 'El usuario no se ha podido registrar' })
@@ -24,7 +23,7 @@ const registerUser = (req, res) => {
 }
 
 const getUsersList = async (req, res) => {
-      const token = req.headers.authorization.replace('Bearer ',(''))
+  const token = req.headers.authorization.replace('Bearer ', (''))
   console.log(token)
   try {
     const decoded = jwt.verify(token, process.env.SECRET);
@@ -47,27 +46,31 @@ const getUsersList = async (req, res) => {
 }
 
 const getUser = async (req, res) => {
-  const token = req.headers.authorization.replace('Bearer ',(''))
+  const token = req.headers.authorization.replace('Bearer ', (''))
   try {
     const decoded = jwt.verify(token, process.env.SECRET);
-    console.log(decoded)
-    if (decoded.role.description === 'admin' || req.params.id === decoded.id) { //|| req.params.id === decoded.._id) {
+    if (decoded.role.description === 'admin' || (req.params.id === decoded.id && !req.body.role)) { //|| req.params.id === decoded.._id) {
       Users
-        .find({ _id: req.params.id })
+        .findByIdAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body }
+        )
         .then((result) => {
-          return res.status(200).json({ sucess: true, message: "operación exitosa", result })
+          Users
+            .findOne({ _id: req.params.id })
+            .then((result) => {
+              return res.status(200).json({ sucess: true, message: 'operación exitosa has editado', result })
+            })
         })
         .catch((error) => {
-          res.json({ success: false, message: "Hubo un error al conectarse a la base de datos, intenta nuevamente" })
+          res.json({ success: false, message: 'Hubo un error al conectarse a la base de datos, intenta nuevamente' })
         })
     } else {
       res.status(403).json({ success: false, message: 'Acceso solo para admin o los datos del mismo usuario acreditado' })
     }
   } catch (err) {
-    return res.status(401).json({ sucess: false, message: "headers authorization no encontradas" })
+    return res.status(401).json({ sucess: false, message: 'headers authorization no encontradas' })
   }
-
-  
 }
 
 
