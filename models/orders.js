6da3f0215f235befc3cf723fa79de58f686e37ff
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-08-08 11:43:50 -0400

    bug8

--- models/orders.js
+++ models/orders.js
@@ -25,7 +25,7 @@ const orderSchema = new Schema({
           type: String,
           required: true,
           unique: false,
-          parse: true
+          sparse: true
         },
         price: {
           type: String,
